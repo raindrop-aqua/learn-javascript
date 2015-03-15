5519d04c2d12e872d321303c764627edbba6e5d7
Masahiro Atsumi <raindrop.aqua@gmail.com>
2015-03-15 14:22:33 +0000

    add self-execute.js

--- books/single-page-web-application/basic/self-execute.js
+++ books/single-page-web-application/basic/self-execute.js
@@ -1,3 +1,4 @@
+// 即時関数（自己実行型無名関数構文）のおさらい
 var prison = (function() {
     var prisoner_name = 'mike mikowski',
         jail_term = '20 years term';
@@ -8,17 +9,18 @@ var prison = (function() {
     };
 })();
 
+// アクセスできない（undefined）
 console.log(prison.prisoner_name);
-
+// returnで返却されたプロパティにはアクセスすることができる
 console.log(prison.prisoner);
-
 console.log(prison.sentence);
 
-
 // --------------------------------
+
 console.log(prison.jail_term);
+// この文を実行した時点で、prison.jail_termというプロパティが作られる
+// 即時関数で設定されたjail_termとは異なることに注意する
 prison.jail_term = 'Sentence commuted.';
 console.log(prison.jail_term);
 
 console.log(prison.prisoner);
-
