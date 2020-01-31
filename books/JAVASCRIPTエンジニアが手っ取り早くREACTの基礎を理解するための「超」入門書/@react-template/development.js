e12f454d028c3272d507ee9f1eb819633db72df1
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-02-01 08:29:05 +0900

    add template

---
+++ books/JAVASCRIPTエンジニアが手っ取り早くREACTの基礎を理解するための「超」入門書/@react-template/development.js
@@ -0,0 +1,36 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const src = path.resolve(__dirname, 'src')
+const dist = path.resolve(__dirname, 'dist')
+
+export default {
+    mode: 'development',
+    entry: src + '/index.jsx',
+
+    output: {
+        path: dist,
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: src + '/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
