801ca56c1cbefb6a82e7d176553936e973079098
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-04-05 23:24:20 +0900

    font/icon sample

--- samples/expo/app01/database-and-sql/App.tsx
+++ samples/expo/app01/database-and-sql/App.tsx
@@ -35,18 +35,35 @@ export default class App extends Component<AppProps, AppState> {
     };
   }
 
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font
+    });
+    this.setState({ isReady: true });
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
     return (
       <Container>
         <Header>
           <Left>
-            <Title>タイトル</Title>
+            <Button transparent>
+              <Icon name="arrow-back" />
+            </Button>
           </Left>
           <Body>
             <Title>タイトル</Title>
           </Body>
           <Right>
-            <Title>タイトル</Title>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
           </Right>
         </Header>
         <Content>
@@ -58,6 +75,22 @@ export default class App extends Component<AppProps, AppState> {
               <Input placeholder="Password" />
             </Item>
           </Form>
+          <Item picker>
+            <Picker
+              mode="dropdown"
+              iosIcon={<Icon name="arrow-down" />}
+              style={{ width: undefined }}
+              placeholder="Select your SIM"
+              placeholderStyle={{ color: "#bfc6ea" }}
+              placeholderIconColor="#007aff"
+            >
+              <Picker.Item label="Wallet" value="key0" />
+              <Picker.Item label="ATM Card" value="key1" />
+              <Picker.Item label="Debit Card" value="key2" />
+              <Picker.Item label="Credit Card" value="key3" />
+              <Picker.Item label="Net Banking" value="key4" />
+            </Picker>
+          </Item>
         </Content>
         <Footer>
           <FooterTab>
@@ -65,18 +98,22 @@ export default class App extends Component<AppProps, AppState> {
               <Badge>
                 <Text>2</Text>
               </Badge>
+              <Icon name="apps" />
               <Text>Apps</Text>
             </Button>
             <Button vertical>
+              <Icon name="camera" />
               <Text>Camera</Text>
             </Button>
             <Button active badge vertical>
               <Badge>
                 <Text>51</Text>
               </Badge>
+              <Icon active name="navigate" />
               <Text>Navigate</Text>
             </Button>
             <Button vertical>
+              <Icon name="person" />
               <Text>Contact</Text>
             </Button>
           </FooterTab>
