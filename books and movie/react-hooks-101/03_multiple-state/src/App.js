bc0d7fe3709e26aeff7b6f6dc7a68d7b2614c9e0
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-01 23:37:03 +0900

    add multiple state

---
+++ books and movie/react-hooks-101/03_multiple-state/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.Name);
+  };
+
+  return (
+    <>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+    </>
+  );
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
+};
+
+export default App;
