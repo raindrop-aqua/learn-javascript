caa6b1e98fa575dbb06b4486f04f94cfd6f4dae8
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-01 23:59:00 +0900

    add object-and-useState

---
+++ books and movie/react-hooks-101/03_object-and-useState/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const App = (props) => {
+  const [state, setState] = useState(props);
+  const { name, price } = state;
+
+  return (
+    <>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
+    </>
+  );
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
+};
+
+export default App;
