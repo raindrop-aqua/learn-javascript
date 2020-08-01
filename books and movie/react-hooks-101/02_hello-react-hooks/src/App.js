9b09bdcd19f696e0725caaab2169a45b78217f9a
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-01 23:26:43 +0900

    add hello react hooks

---
+++ books and movie/react-hooks-101/02_hello-react-hooks/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+  const reset = () => setCount(0);
+  const double = () => setCount(count * 2);
+  const divide3 = () =>
+    setCount((previousCount) =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+
+  return (
+    <>
+      <div>Count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>3の倍数のときだけ3で割る</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
