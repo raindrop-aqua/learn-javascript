a3a7051558a7febd73186ef11ba4b09a465667b4
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-02 01:06:39 +0900

    use-reducer-with-useReducer

---
+++ books and movie/react-hooks-101/08_use-reducer-with-useReducer/src/reducers/index.js
@@ -0,0 +1,17 @@
+const events = (state = [], action) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      const event = { title: action.title, body: action.body };
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, { id, ...event }];
+    case "DELETE_EVENT":
+      return state;
+    case "DELETE_ALL_EVENTS":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default events;
