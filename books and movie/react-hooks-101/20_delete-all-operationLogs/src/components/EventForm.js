fd636f25fabd1c9cf807a05baaca8518a1040be6
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-02 14:57:01 +0900

    delete all operation logs

---
+++ books and movie/react-hooks-101/20_delete-all-operationLogs/src/components/EventForm.js
@@ -0,0 +1,120 @@
+import React, { useContext, useState } from "react";
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS,
+} from "../actions";
+import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils";
+
+const EventForm = () => {
+  const { state, dispatch } = useContext(AppContext);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  /** イベントを作成する */
+  const addEvent = (e) => {
+    dispatch({
+      type: CREATE_EVENT,
+      title,
+      body,
+    });
+
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: "イベントを作成しました。",
+      operatedAt: timeCurrentIso8601(),
+    });
+
+    setTitle("");
+    setBody("");
+  };
+
+  /** 全てのイベントを削除する */
+  const deleteAllEvents = (e) => {
+    const result = window.confirm(
+      "全てのイベントを本当に削除しても良いですか？"
+    );
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: "全てのイベントを削除しました。",
+        operatedAt: timeCurrentIso8601(),
+      });
+    }
+  };
+
+  /** すべての操作ログを削除する */
+  const deleteAllOperationLogs = (e) => {
+    const result = window.confirm(
+      "全ての操作ログを本当に削除しても良いですか？"
+    );
+    if (result) {
+      dispatch({ type: DELETE_ALL_OPERATION_LOGS });
+    }
+  };
+
+  /** 「イベントを作成する」ボタン活性化状態 */
+  const unCreatable = title === "" || body === "";
+
+  /** 「全てのイベントを削除する」ボタン活性化状態 */
+  const unAllDeletableEvents = state.events.length === 0;
+
+  /** 「全ての操作ログを削除する」ボタン活性化状態 */
+  const unAllDeletableOperationLogs = state.operationLogs.length === 0;
+
+  return (
+    <>
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル </label>
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディ </label>
+          <input
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={addEvent}
+          disabled={unCreatable}
+        >
+          イベントを作成する
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+          disabled={unAllDeletableEvents}
+        >
+          全てのイベントを削除する
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={deleteAllOperationLogs}
+          disabled={unAllDeletableOperationLogs}
+        >
+          全ての操作ログを削除する
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default EventForm;
