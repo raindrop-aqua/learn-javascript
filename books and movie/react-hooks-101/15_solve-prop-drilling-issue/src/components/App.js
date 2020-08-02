8023c608301c993a93b440ff534474a4ac38cab6
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-02 12:54:07 +0900

    solve prop driling issue

---
+++ books and movie/react-hooks-101/15_solve-prop-drilling-issue/src/components/App.js
@@ -0,0 +1,22 @@
+import React, { useReducer } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import EventForm from "./EventForm";
+import EventList from "./EventList";
+import AppContext from "../contexts/AppContext";
+import reducer from "../reducers";
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      <div className="container-fluid">
+        <EventForm />
+        <EventList />
+      </div>
+    </AppContext.Provider>
+  );
+};
+
+export default App;
---
+++ books and movie/react-hooks-101/15_solve-prop-drilling-issue/src/components/EventList.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import Event from "./Event";
+import AppContext from "../contexts/AppContext";
+
+const EventList = () => {
+  const { state } = useContext(AppContext);
+
+  return (
+    <>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディ</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map((event, index) => (
+            <Event key={index} event={event} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default EventList;
