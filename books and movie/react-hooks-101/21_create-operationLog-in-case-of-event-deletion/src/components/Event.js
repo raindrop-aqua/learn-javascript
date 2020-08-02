924c282e222f6ff66bb749db7dcbcb82ffb29aa7
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-02 15:04:53 +0900

    create-operationLog-in-case-of-event-deletion

--- books and movie/react-hooks-101/21_create-operationLog-in-case-of-event-deletion/src/components/Event.js
+++ books and movie/react-hooks-101/21_create-operationLog-in-case-of-event-deletion/src/components/Event.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { DELETE_EVENT } from "../actions";
+import { ADD_OPERATION_LOG, DELETE_EVENT } from "../actions";
 import AppContext from "../contexts/AppContext";
+import { timeCurrentIso8601 } from "../utils";
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext);
@@ -13,6 +14,11 @@ const Event = ({ event }) => {
     );
     if (result) {
       dispatch({ type: DELETE_EVENT, id });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `イベント(id=${id})を削除しました。`,
+        operatedAt: timeCurrentIso8601(),
+      });
     }
   };
 
