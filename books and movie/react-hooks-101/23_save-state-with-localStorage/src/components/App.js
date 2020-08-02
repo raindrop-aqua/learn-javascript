8d30e209bcfb63cc63510d3d510073fa81bc8f69
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-02 15:32:37 +0900

    fix save-state-with-localStorage

--- books and movie/react-hooks-101/23_save-state-with-localStorage/src/components/App.js
+++ books and movie/react-hooks-101/23_save-state-with-localStorage/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import EventForm from "./EventForm";
@@ -7,13 +7,21 @@ import OperationLogList from "./OperationLogList";
 import AppContext from "../contexts/AppContext";
 import reducer from "../reducers";
 
-const App = () => {
-  const initialState = {
-    events: [],
-    operationLogs: [],
-  };
+const APP_KEY = "react-hooks-101";
 
+const App = () => {
+  const appState = localStorage.getItem(APP_KEY);
+  const initialState = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
