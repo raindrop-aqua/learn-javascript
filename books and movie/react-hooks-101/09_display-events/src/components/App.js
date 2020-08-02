162424e2bfb38bc09070c9f2659dd1ccebbb6932
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-02 09:00:19 +0900

    display-event

---
+++ books and movie/react-hooks-101/09_display-events/src/components/App.js
@@ -0,0 +1,72 @@
+import React, { useReducer, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Event from "./Event";
+import reducer from "../reducers";
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addEvent = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: "CREATE_EVENT",
+      title,
+      body,
+    });
+
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル </label>
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディ </label>
+          <input
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </div>
+
+        <button className="btn btn-primary" onClick={addEvent}>
+          イベントを作成する
+        </button>
+        <button className="btn btn-danger">全てのイベントを削除する</button>
+      </form>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディ</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map((event, index) => (
+            <Event key={index} event={event} dispatch={dispatch} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
