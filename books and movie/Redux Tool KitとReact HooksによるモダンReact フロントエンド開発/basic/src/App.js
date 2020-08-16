9a0b840a76608d630890fe833d5a3f855c0cf08f
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-08-16 09:57:41 +0900

    initial commit.

---
+++ books and movie/Redux Tool KitとReact HooksによるモダンReact フロントエンド開発/basic/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
