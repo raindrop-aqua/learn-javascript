cd5827d131aa1c1340e329e778cdbc25adced4bc
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-03 18:31:31 +0900

    section01 finished.

---
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section01/README.md
@@ -0,0 +1,3 @@
+# Re-vents
+
+https://www.udemy.com/course/build-an-app-with-react-redux-and-firestore-from-scratch/
---
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section01/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
+
+const rootEl = document.getElementById("root");
+
+const render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App", function () {
+    setTimeout(render);
+  });
+}
+
+render();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
