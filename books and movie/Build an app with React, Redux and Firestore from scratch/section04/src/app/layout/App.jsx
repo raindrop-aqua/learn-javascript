30c841071695eaafb3555eb62467bfbd5c023368
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-06 16:20:41 +0900

    section4 first commit

---
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section04/src/app/layout/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+import { Container } from "semantic-ui-react";
+
+function App() {
+  const [formOpen, setFormOpen] = useState(true);
+
+  return (
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section04/src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventListItem from "./EventListItem";
+
+export default function EventList(props) {
+  return (
+    <>
+      {props.events.map((event) => (
+        <EventListItem key={event.id} event={event} />
+      ))}
+    </>
+  );
+}
