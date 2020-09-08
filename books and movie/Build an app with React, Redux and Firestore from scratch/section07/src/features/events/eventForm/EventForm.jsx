a3213472892202cc837e0d35eb3779a9fddb5a77
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-09 01:42:15 +0900

    replace formik field

--- books and movie/Build an app with React, Redux and Firestore from scratch/section07/src/features/events/eventForm/EventForm.jsx
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section07/src/features/events/eventForm/EventForm.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import cuid from "cuid";
-import { Segment, Header, Form, Button } from "semantic-ui-react";
-import { Formik } from "formik";
+import { Segment, Header, Button, FormField } from "semantic-ui-react";
+import { Formik, Form, Field } from "formik";
 import { updateEvent, createEvent } from "../eventActions";
 
 export default function EventForm({ match, history }) {
@@ -50,73 +50,35 @@ export default function EventForm({ match, history }) {
         initialValues={initialValues}
         onSubmit={(values) => console.log(values)}
       >
-        {({ values, handleChange, handleSubmit }) => (
-          <Form onSubmit={handleSubmit}>
-            <Form.Field>
-              <input
-                type='text'
-                placeholder='Event title'
-                name='title'
-                value={values.title}
-                onChange={handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <input
-                type='text'
-                placeholder='Category'
-                name='category'
-                value={values.category}
-                onChange={handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <input
-                type='text'
-                placeholder='Description'
-                name='description'
-                value={values.description}
-                onChange={handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <input
-                type='text'
-                placeholder='City'
-                name='city'
-                value={values.city}
-                onChange={handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <input
-                type='text'
-                placeholder='Venue'
-                name='venue'
-                value={values.venue}
-                onChange={handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <input
-                type='date'
-                placeholder='Date'
-                name='date'
-                value={values.date}
-                onChange={handleChange}
-              />
-            </Form.Field>
+        <Form className='ui form'>
+          <FormField>
+            <Field name='title' placeholder='Event title' />
+          </FormField>
+          <FormField>
+            <Field name='category' placeholder='Category' />
+          </FormField>
+          <FormField>
+            <Field name='description' placeholder='Description' />
+          </FormField>
+          <FormField>
+            <Field name='city' placeholder='City' />
+          </FormField>
+          <FormField>
+            <Field name='venue' placeholder='Venue title' />
+          </FormField>
+          <FormField>
+            <Field name='date' placeholder='Event date' type='date' />
+          </FormField>
 
-            <Button type='submit' floated='right' positive content='Submit' />
-            <Button
-              as={Link}
-              to='/events'
-              type='submit'
-              floated='right'
-              content='Cancel'
-            />
-          </Form>
-        )}
+          <Button type='submit' floated='right' positive content='Submit' />
+          <Button
+            as={Link}
+            to='/events'
+            type='submit'
+            floated='right'
+            content='Cancel'
+          />
+        </Form>
       </Formik>
     </Segment>
   );
