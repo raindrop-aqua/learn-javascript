38b1967436d0088cc156ae12c496ec4e62967c4b
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-10 23:16:37 +0900

    data load on async

--- books and movie/Build an app with React, Redux and Firestore from scratch/section09/src/features/events/eventActions.js
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section09/src/features/events/eventActions.js
@@ -1,4 +1,30 @@
-import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from "./eventConstants";
+import {
+  CREATE_EVENT,
+  UPDATE_EVENT,
+  DELETE_EVENT,
+  FETCH_EVENTS,
+} from "./eventConstants";
+import {
+  asyncActionStart,
+  asyncActionError,
+  asyncActionFinish,
+} from "../../app/async/asyncReducer";
+import { fetchSampleData } from "../../app/api/mockApi";
+
+export function loadEvents() {
+  return async function (dispatch) {
+    dispatch(asyncActionStart());
+    try {
+      const events = await fetchSampleData();
+      console.log(events);
+      dispatch({ type: FETCH_EVENTS, payload: events });
+      dispatch(asyncActionFinish());
+    } catch (error) {
+      console.log(error);
+      dispatch(asyncActionError(error));
+    }
+  };
+}
 
 export function createEvent(event) {
   return {
--- books and movie/Build an app with React, Redux and Firestore from scratch/section09/src/index.js
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section09/src/index.js
@@ -9,9 +9,11 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
 import ScrollToTop from "./app/layout/ScrollToTop";
+import { loadEvents } from "./features/events/eventActions";
 
-const store = configureStore();
 const rootEl = document.getElementById("root");
+const store = configureStore();
+store.dispatch(loadEvents());
 
 const render = () => {
   ReactDOM.render(
