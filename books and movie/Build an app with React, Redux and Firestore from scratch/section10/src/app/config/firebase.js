6909b60613654273739ded6927c6411a8b7b42e2
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-11 00:20:25 +0900

    install firebase

--- books and movie/Build an app with React, Redux and Firestore from scratch/section10/src/app/config/firebase.js
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section10/src/app/config/firebase.js
@@ -1,3 +1,9 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA-3fBfUCJq0jiPhhXIgnPjuWc8NIx-4lM",
   authDomain: "revents-course-31863.firebaseapp.com",
@@ -7,3 +13,8 @@ const firebaseConfig = {
   messagingSenderId: "436704824948",
   appId: "1:436704824948:web:6e59037a569f66fdaccb21",
 };
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
