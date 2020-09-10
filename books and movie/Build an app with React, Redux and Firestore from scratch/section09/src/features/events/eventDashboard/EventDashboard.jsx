5710ec72d1c0e983e8d994ed10401fe206bc8606
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-10 23:37:31 +0900

    loading impl.

--- books and movie/Build an app with React, Redux and Firestore from scratch/section09/src/features/events/eventDashboard/EventDashboard.jsx
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section09/src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
-import { useSelector } from "react-redux";
+import EventListItemPlaceholder from "./EventListItemPlaceholder";
 
 export default function EventDashboard() {
-  const events = useSelector((state) => state.event.events);
+  const { events } = useSelector((state) => state.event);
+  const { loading } = useSelector((state) => state.async);
 
   return (
     <Grid>
       <Grid.Column width={10}>
+        {loading && (
+          <>
+            <EventListItemPlaceholder />
+            <EventListItemPlaceholder />
+          </>
+        )}
+
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
