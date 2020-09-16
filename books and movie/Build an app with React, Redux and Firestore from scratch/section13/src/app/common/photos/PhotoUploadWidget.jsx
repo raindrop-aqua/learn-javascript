5b30c4ac4ebfebe341dde05e16f0fc2d05d0f19e
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-16 19:10:03 +0900

    dropzone impl.

--- books and movie/Build an app with React, Redux and Firestore from scratch/section13/src/app/common/photos/PhotoUploadWidget.jsx
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section13/src/app/common/photos/PhotoUploadWidget.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Header } from "semantic-ui-react";
+import PhotoWidgetDropZone from "./PhotoWidgetDropZone";
 
 export default function PhotoUploadWidget() {
+  const [files, setFiles] = useState([]);
+
   return (
     <Grid>
       <Grid.Column width={4}>
         <Header color='teal' sub content='Step 1 - Add Photo' />
+        <PhotoWidgetDropZone setFiles={setFiles} />
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
