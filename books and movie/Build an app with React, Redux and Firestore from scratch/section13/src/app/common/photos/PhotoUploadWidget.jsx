32cb27ed1ac1b7d78160f31d54653304c7b4b2e6
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-16 18:47:34 +0900

    add photos tab

---
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section13/src/app/common/photos/PhotoUploadWidget.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Grid, Header } from "semantic-ui-react";
+
+export default function PhotoUploadWidget() {
+  return (
+    <Grid>
+      <Grid.Column width={4}>
+        <Header color='teal' sub content='Step 1 - Add Photo' />
+      </Grid.Column>
+      <Grid.Column width={1} />
+      <Grid.Column width={4}>
+        <Header color='teal' sub content='Step 2 - Resize' />
+      </Grid.Column>
+      <Grid.Column width={1} />
+      <Grid.Column width={4}>
+        <Header color='teal' sub content='Step 3 - Preview & upload' />
+      </Grid.Column>
+    </Grid>
+  );
+}
---
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section13/src/features/profiles/profilePage/PhotosTab.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Grid, Header, Button, Tab, Card, Image } from "semantic-ui-react";
+import PhotoUploadWidget from "../../../app/common/photos/PhotoUploadWidget";
+
+export default function PhotosTab({ profile, isCurrentUser }) {
+  const [editMode, setEditMode] = useState(true);
+
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated='left' icon='user' content={"Photos"} />
+          {isCurrentUser && (
+            <Button
+              onClick={() => setEditMode(!editMode)}
+              floated='right'
+              basic
+              content={editMode ? "Cancel" : "Add Photo"}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {editMode ? (
+            <PhotoUploadWidget />
+          ) : (
+            <>
+              <Card.Group itemsPerRow={5}>
+                <Card>
+                  <Image src={"/assets/user.png"} />
+                  <Button.Group fluid widths={2}>
+                    <Button basic color='green' content='Main' />
+                    <Button basic color='red' icon='trash' />
+                  </Button.Group>
+                </Card>
+              </Card.Group>
+            </>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+}
