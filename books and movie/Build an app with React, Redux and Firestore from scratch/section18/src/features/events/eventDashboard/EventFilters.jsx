2765877ef1de213c9d2f2312e6e59bcd7968e20c
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-09-21 23:08:43 +0900

    End of section18

--- books and movie/Build an app with React, Redux and Firestore from scratch/section18/src/features/events/eventDashboard/EventFilters.jsx
+++ books and movie/Build an app with React, Redux and Firestore from scratch/section18/src/features/events/eventDashboard/EventFilters.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import { Menu, Header } from "semantic-ui-react";
 import Calendar from "react-calendar";
+import { useSelector } from "react-redux";
 
 export default function EventFilters({ setPredicate, predicate, loading }) {
+  const { authenticated } = useSelector((state) => state.auth);
+
   return (
     <>
-      <Menu vertical size='large' style={{ width: "100%" }}>
-        <Header icon='filter' attached color='teal' content='Filters' />
-        <Menu.Item
-          content='All Events'
-          active={predicate.get("filter") === "all"}
-          onClick={() => setPredicate("filter", "all")}
-          disabled={loading}
-        />
-        <Menu.Item
-          content="I'm going"
-          active={predicate.get("filter") === "isGoing"}
-          onClick={() => setPredicate("filter", "isGoing")}
-          disabled={loading}
-        />
-        <Menu.Item
-          content="I'm hosting"
-          active={predicate.get("filter") === "isHost"}
-          onClick={() => setPredicate("filter", "isHost")}
-          disabled={loading}
-        />
-      </Menu>
+      {authenticated && (
+        <Menu vertical size='large' style={{ width: "100%" }}>
+          <Header icon='filter' attached color='teal' content='Filters' />
+          <Menu.Item
+            content='All Events'
+            active={predicate.get("filter") === "all"}
+            onClick={() => setPredicate("filter", "all")}
+            disabled={loading}
+          />
+          <Menu.Item
+            content="I'm going"
+            active={predicate.get("filter") === "isGoing"}
+            onClick={() => setPredicate("filter", "isGoing")}
+            disabled={loading}
+          />
+          <Menu.Item
+            content="I'm hosting"
+            active={predicate.get("filter") === "isHost"}
+            onClick={() => setPredicate("filter", "isHost")}
+            disabled={loading}
+          />
+        </Menu>
+      )}
       <Header icon='calendar' attached color='teal' content='Select date' />
       <Calendar
         onChange={(date) => setPredicate("startDate", date)}
